78fae7698848866487a26d1308625556affb4d35
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-12 19:50:40 +0530

    video card updated

--- src/components/VideoCards.jsx
+++ src/components/VideoCards.jsx
@@ -6,12 +6,12 @@ const VideoCards = ({ info }) => {
   const { channelTitle, title, thumbnails } = snippet;
 
   return (
-    <div className="p-2 m-2 w-72 shadow-lg">
+    <div className="p-2 m-2 w-72 h-80 shadow-lg">
       <img className="rounded-lg" src={thumbnails.medium.url} alt="thumbnail" />
       <ul>
         <li className="font-bold py-2">{title}</li>
         <li>{channelTitle}</li>
-        <li>{statistics.viewCount}</li>
+        <li>{statistics.viewCount} views</li>
       </ul>
     </div>
   );
